9871ce3442a300a0c6ba29e2d17d174376503fee
gyq0211 <56936934+gyq0211@users.noreply.github.com>
2020-04-15 22:41:55 -0400

    Update todo1.py

--- Spark Streaming/todo1.py
+++ Spark Streaming/todo1.py
@@ -11,10 +11,7 @@ access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-# , access_token_secret
-#auth = tweepy.OAuthHandler(_creds["consumer_key"], _creds["consumer_secret"]) auth.set_access_token(_creds["access_token"], _creds["access_token_secret"]) del _creds # delete _creds immediately!
 API = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-#API = tweepy.API(auth)
 
 def show_tweet(tweet):
     try:
@@ -26,42 +23,16 @@ def show_tweet(tweet):
 
 
 public_tweets = API.home_timeline()
-#for tweet in public_tweets[:1]:
-    #print(show_tweet(tweet))
 
-
-"""class Listener(tweepy.streaming.StreamListener):
-
-    def __init__(self, output_file=sys.stdout):
-        super(Listener, self).__init__()
-        self.output_file = output_file
-
-    def on_status(self, tweet):
-        print(show_tweet(tweet), file=self.output_file)
-
-    def on_error(self, status_code):
-        print(status_code)
-        return False
-
-
-listener = Listener()"""
-#stream = tweepy.Stream(auth=API.auth, listener=listener)
 print('Starting!!!!!!!!!!!!!!!!!!')
-# stream.filter(follow=[nyt.id_str])
 
 
 try:
-    #data = open("gs://bigdata-quiz4/todo1.txt", 'w+')
-    #data = open("/home/yunqigaoyg/outp1.txt", 'w+')
     print('Start streaming.')
     tweeters = API.followers(id="nytimes")
-    #stream.filter(follow=tweeters)
     for i in range(20):
-        #print(tweeters[i])
         print(tweeters[i])
-    #data.close()
 except KeyboardInterrupt:
     print("Stopped.")
 finally:
-    #stream.disconnect()
     print('Done.')
